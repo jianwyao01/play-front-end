b8f926237051ea2e5f5448b8465b7bf5d02b20c0
willyao <wyao314320@gmail.com>
2025-03-13 11:34:14 +0800

    feat: enhance createApp function with reducers and event dispatching

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 import {destroyDOM} from "./destroy-dom";
+import {Dispatcher} from "./dispatcher";
 import {mountDOM} from "./mount-dom";
 
-export function createApp({state, view}) {
+export function createApp({state, view, reducers}) {
     let parentEl = null;
     let vdom = null;
+
+    const dispatcher = new Dispatcher();
+    const subscriptions = [dispatcher.afterEveryCommand(renderApp)];
+
+    function emit(eventName, payload) {
+        dispatcher.dispatch(eventName, payload);
+    }
+
+    for (const eventName of reducers) {
+        const reducer = reducers[eventName];
+
+        const subs = dispatcher.subscribe(eventName, (payload) => {
+            state = reducer(state, payload);
+        });
+        subscriptions.push(subs);
+    }
 
     function renderApp() {
         if (vdom) {
             destroyDOM(vdom);
         }
 
-        vdom = view(state);
+        vdom = view(state, emit);
         mountDOM(vdom, parentEl);
     }
 
@@ -18,6 +35,11 @@ export function createApp({state, view}) {
         mount(_parentEl) {
             parentEl = _parentEl;
             renderApp();
+        },
+        unmount() {
+            destroyDOM(vdom);
+            vdom = null;
+            subscriptions.forEach((unsubscribe) => unsubscribe());
         }
     }
 }
